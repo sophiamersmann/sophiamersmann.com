de857b2f626562c177b197b38dd19dc4bb43925c
sophiamersmann <sophia.mersmann1@gmail.com>
2023-02-22 17:43:37 +0100

    link to previous deployments on Vercel

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,17 @@
 import type { PageServerLoad } from './$types';
-import { GITHUB_PAT } from '$env/static/private';
+import { GITHUB_PAT, VERCEL_TOKEN } from '$env/static/private';
 
 import projects from '$lib/load-projects';
 
 export type Commit = {
+	// GitHub's commit SHA
 	sha: string;
 	shaShort: string;
-	url: string;
+	// GitHub's commit URL
+	githubUrl: string;
+	// Vercel's deployment URL
+	url: string | null;
+	// commit message
 	message: string;
 	date: Date;
 };
@@ -40,16 +45,71 @@ async function fetchGitCommits({
 	}
 }
 
-export const load = (async () => {
-	const commits: Commit[] = [];
+async function fetchVercelDeployments({
+	projectId,
+	state,
+	target,
+}: {
+	projectId: string;
+	state: string;
+	target: string;
+}): Promise<{ deployments: Record<string, any>[] } | null> {
+	const base = 'https://api.vercel.com';
+	const url = `${base}/v6/deployments?projectId=${projectId}&state=${state}&target=${target}`;
 
+	try {
+		const response = await fetch(url, {
+			headers: {
+				Authorization: `Bearer ${VERCEL_TOKEN}`,
+			},
+		});
+
+		if (response.ok) {
+			return await response.json();
+		} else {
+			return null;
+		}
+	} catch {
+		return null;
+	}
+}
+
+export const load = (async () => {
 	// fetch commits from GitHub API
-	const fetchedCommits = await fetchGitCommits({
+	const promisedCommits = fetchGitCommits({
 		owner: 'sophiamersmann',
 		repo: 'sophiamersmann.com',
 	});
 
+	// fetch previous deployment URLs from Vercel API
+	const promisedDeployments = fetchVercelDeployments({
+		projectId: 'prj_mY89CUR6QfCPQOQuDhyxL0oHlj2C',
+		state: 'READY',
+		target: 'production',
+	});
+
+	const [fetchedCommits, fetchedDeployments] = await Promise.all([
+		promisedCommits,
+		promisedDeployments,
+	]);
+
+	const commits: Commit[] = [];
 	if (fetchedCommits != null) {
+		let deploymentUrlMap = new Map<string, string>();
+
+		// map from a GitHub commit SHA to a Vercel deployment URL
+		if (fetchedDeployments != null) {
+			const deployments = fetchedDeployments.deployments.map(
+				(deployment: Record<string, any>) => ({
+					url: deployment.url,
+					githubCommitSha: deployment.meta.githubCommitSha,
+				})
+			);
+			deploymentUrlMap = new Map(
+				deployments.map((d) => [d.githubCommitSha, d.url])
+			);
+		}
+
 		for (const c of fetchedCommits) {
 			let message = c.commit.message.split('\n')[0];
 
@@ -60,9 +120,12 @@ export const load = (async () => {
 				commits.push({
 					sha: c.sha,
 					shaShort: c.sha.slice(0, 7),
-					url: c.html_url,
+					githubUrl: c.html_url,
 					message,
 					date: new Date(c.commit.author.date),
+					url: deploymentUrlMap.has(c.sha)
+						? 'https://' + deploymentUrlMap.get(c.sha)
+						: null,
 				});
 			}
 		}
